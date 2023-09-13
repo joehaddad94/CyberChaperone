7bb35077d13e2f21d4b22a5d433ec144bf3a97ce
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-13 19:16:45 +0300

    server <> Created users migrations

--- server-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ server-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('type_id')->constrained('user_types');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('device_token')->nullable();
             $table->timestamps();
         });
     }
