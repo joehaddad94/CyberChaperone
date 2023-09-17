24d9684da4aa80b4812f0d204982536e467fb981
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-17 15:33:58 +0300

    client <> Created swipeCalendar file and added some dependancies

---
+++ client-RN/Components/SwipeCalendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native'
+
+const SwipeCalendar: React.FC = () => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default SwipeCalendar;
