ceb2ea4832ede07b911ecf926d74b315bb7a36eb
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-20 01:30:45 +0300

    client <> Added header to info screen

--- client-RN/Screens/InfoScreen.tsx
+++ client-RN/Screens/InfoScreen.tsx
@@ -1,9 +1,14 @@
 import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
 import globalStyles from '../styles';
+import Header from '../Components/Header'
+
 
 export default function InfoScreen() {
     return(
         <View style={[globalStyles.container, globalStyles.primaryColor]}>
+            <Header 
+                headerTitle={"Info"}
+            />
             <Text>
                 Info Screen!!!
             </Text>
