29b3a3a4b1fc23442f8a75afe22ef1e405b1e89b
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-20 01:18:10 +0300

    server <> Fixing emotions data average.

--- server-laravel/server.js
+++ server-laravel/server.js
@@ -20,23 +20,63 @@ io.on("connection", (socket) => {
       });
 
       socket.on("emotions-data", async (emotions) => {
-        console.log(emotions)
-    //     try {
-    //         const apiUrl = "";
+        // console.log(emotions)
+        try {
+            const apiUrl = "";
 
-    //         const headers = {
-    //             Authorization: `Bearer ${userToken}`,
-    //         };
+            const headers = {
+                Authorization: `Bearer ${userToken}`,
+            };
 
-    //         const response = await axios.post(apiUrl, emotions, { headers });
+            const parsedEmotions = JSON.parse(emotions);
+            // console.log(parsedEmotions)
 
-    //         console.log("Emotion data sent successfully:", response.data);
-    //     } catch (error) {
-    //         console.error("Error sending emotion data:", error);
-    //     }
+            // const averageEmotions = calculateAverageEmotions(parsedEmotions);
+            const averageEmotions = calculateAverageEmotions(emotions);
+            console.log(averageEmotions)
+
+            // const response = await axios.post(apiUrl, averageEmotions, { headers });
+
+            // console.log("Average emotion data sent successfully:", response.data);
+        } catch (error) {
+            console.error("Error sending average emotion data:", error);
+        }
+    });
+});
+
+function calculateAverageEmotions(emotionsData) {
+    const emotionTotals = {
+        Neutral: 0,
+        Happy: 0,
+        Sad: 0,
+        Angry: 0,
+        Fearful: 0,
+        Disgusted: 0,
+        Surprised: 0,
+    };
+
+    emotionsData.forEach((emotion) => {
+
+        const parsedEmotion = JSON.parse(emotion);
+
+        for (const emotionName in parsedEmotion) {
+            if (emotionTotals.hasOwnProperty(emotionName)) {
+                emotionTotals[emotionName] += parseFloat(parsedEmotion[emotionName]);
+            }
+        }
     });
 
-});
+    const numEmotions = emotionsData.length;
+    const averageEmotions = {};
+
+    for (const emotionName in emotionTotals) {
+        if (emotionTotals.hasOwnProperty(emotionName)) {
+            averageEmotions[emotionName] = (emotionTotals[emotionName] / numEmotions).toFixed(2);
+        }
+    }
+
+    return averageEmotions;
+}
 
 httpServer.listen(3000, () => {
   console.log('Server is running on port 3000');
