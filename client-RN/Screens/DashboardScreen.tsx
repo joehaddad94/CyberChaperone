9da821e56b218f44e6bb305ffbc13d04940995bd
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-19 08:49:40 +0300

    client <> Adding background.

--- client-RN/Screens/DashboardScreen.tsx
+++ client-RN/Screens/DashboardScreen.tsx
@@ -1,11 +1,16 @@
 import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
 import SwipeCalendar from '../Components/SwipeCalendar';
+import globalStyles from '../styles';
 
 
 export default function DashboardScreen() {
     return(
-    
-        <SwipeCalendar/>
+        <View
+              style={[globalStyles.backgroundImage,globalStyles.primaryColor]}
+            >
+            <SwipeCalendar/>
+        </View>
+        
         
     )
 }
