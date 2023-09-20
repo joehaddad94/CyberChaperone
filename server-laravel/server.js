777c2b59efee29ad1fe2fa7994878364694fc2af
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-20 12:50:49 +0300

    server <> Fixing average of emotions

--- server-laravel/server.js
+++ server-laravel/server.js
@@ -6,74 +6,6 @@ let userToken;
 let accumulatedEmotions = [];
 let calculateAverageTimer;
 
-let emotionsArray = [
-    {
-        emotions: [
-          '{"emotion":"Neutral","percentage":"1.85"}',
-          '{"emotion":"Happy","percentage":"98.05"}',
-          '{"emotion":"Sad","percentage":"0.04"}',
-          '{"emotion":"Angry","percentage":"0.00"}',
-          '{"emotion":"Fearful","percentage":"0.00"}',
-          '{"emotion":"Disgusted","percentage":"0.01"}',
-          '{"emotion":"Surprised","percentage":"0.05"}'
-        ]
-      },
-      {
-        emotions: [
-          '{"emotion":"Neutral","percentage":"1.85"}',
-          '{"emotion":"Happy","percentage":"98.05"}',
-          '{"emotion":"Sad","percentage":"0.04"}',
-          '{"emotion":"Angry","percentage":"0.00"}',
-          '{"emotion":"Fearful","percentage":"0.00"}',
-          '{"emotion":"Disgusted","percentage":"0.01"}',
-          '{"emotion":"Surprised","percentage":"0.05"}'
-        ]
-      },
-      {
-        emotions: [
-          '{"emotion":"Neutral","percentage":"1.88"}',
-          '{"emotion":"Happy","percentage":"98.01"}',
-          '{"emotion":"Sad","percentage":"0.05"}',
-          '{"emotion":"Angry","percentage":"0.00"}',
-          '{"emotion":"Fearful","percentage":"0.00"}',
-          '{"emotion":"Disgusted","percentage":"0.02"}',
-          '{"emotion":"Surprised","percentage":"0.02"}'
-        ]
-      },
-]
-
-
-let emotionsArray1 = [
-        { emotion: 'Fearful', percentage: '0.00' },
-        { emotion: 'Disgusted', percentage: '0.00' },
-        { emotion: 'Surprised', percentage: '0.04' },
-        { emotion: 'Neutral', percentage: '15.44' },
-        { emotion: 'Happy', percentage: '84.42' },
-        { emotion: 'Sad', percentage: '0.01' },
-        { emotion: 'Angry', percentage: '0.03' },
-        { emotion: 'Fearful', percentage: '0.00' },
-        { emotion: 'Disgusted', percentage: '0.02' },
-        { emotion: 'Surprised', percentage: '0.09' },
-        { emotion: 'Neutral', percentage: '5.25' },
-        { emotion: 'Happy', percentage: '94.71' },
-        { emotion: 'Sad', percentage: '0.00' },
-        { emotion: 'Angry', percentage: '0.00' },
-        { emotion: 'Fearful', percentage: '0.00' },
-        { emotion: 'Disgusted', percentage: '0.00' },
-        { emotion: 'Surprised', percentage: '0.04' },
-        { emotion: 'Neutral', percentage: '5.02' },
-        { emotion: 'Happy', percentage: '94.93' },
-        { emotion: 'Sad', percentage: '0.00' },
-        { emotion: 'Angry', percentage: '0.00' },
-        { emotion: 'Fearful', percentage: '0.00' },
-        { emotion: 'Disgusted', percentage: '0.00' },
-        { emotion: 'Surprised', percentage: '0.04' },
-        { emotion: 'Neutral', percentage: '10.09' },
-        { emotion: 'Happy', percentage: '89.86' },
-        { emotion: 'Sad', percentage: '0.00' },
-        { emotion: 'Angry', percentage: '0.01' },
-]
-
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
@@ -98,11 +30,9 @@ io.on("connection", (socket) => {
                 Authorization: `Bearer ${userToken}`,
             };
 
-            // const averageEmotions = calculateEmotionAverages(emotions);
-            // console.log(averageEmotions)
             accumulatedEmotions.push(...emotions);
 
-        if (!calculateAverageTimer) {
+        if (accumulatedEmotions.length > 0 && !calculateAverageTimer) {
             calculateAverageTimer = setInterval(() => {
                 const averageEmotions = calculateEmotionAverages(accumulatedEmotions);
                 accumulatedEmotions = [];
@@ -119,18 +49,15 @@ io.on("connection", (socket) => {
 });
 
 function calculateEmotionAverages(emotionsArray) {
-    // Initialize an object to store the sum and count for each emotion
     const emotionSum = {};
     const emotionCount = {};
 
-    // Iterate through the emotionsArray and parse the JSON strings to calculate the sum and count
     emotionsArray.forEach((entry) => {
         entry.emotions.forEach((emotionStr) => {
             const emotionObj = JSON.parse(emotionStr);
             const { emotion, percentage } = emotionObj;
             const percentageValue = parseFloat(percentage);
 
-            // Update the sum and count for each emotion
             if (!emotionSum[emotion]) {
                 emotionSum[emotion] = 0;
                 emotionCount[emotion] = 0;
@@ -140,14 +67,12 @@ function calculateEmotionAverages(emotionsArray) {
         });
     });
 
-    // Calculate the average for each emotion
     const emotionAverages = {};
     for (const emotion in emotionSum) {
         const average = emotionSum[emotion] / emotionCount[emotion];
         emotionAverages[emotion] = Math.round(average * 100) / 100;
     }
 
-    // Get the timestamp from the first entry (assuming they all have the same timestamp)
     const currentTimestamp = new Date().toISOString();
 
     return { timestamp: currentTimestamp, emotionAverages };
