eea20e5cef1713d76bf2807e870384200b6ccd76
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-20 18:32:53 +0300

    client <> Working on the profile screen

--- client-RN/Screens/ProfileScreen.tsx
+++ client-RN/Screens/ProfileScreen.tsx
@@ -1,23 +1,68 @@
-import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ImageBackground, Image, StyleSheet, View } from 'react-native';
 import globalStyles from '../styles';
-import Header from '../Components/Header'
+import Header from '../Components/Header';
+import PressableTitle from '../Components/PressableTitle'; // Import the new component
 
 export default function ProfileScreen() {
-    return(
-            <ImageBackground
-              source={require('../assets/images/DarkBG.png')}
-              style={globalStyles.backgroundImage}
-              resizeMode="cover"
-            >
-              <Header 
-                headerTitle={"Profile"}
-                backgroundColor= '#00B69B'
-            />
-              
-              <View style={[globalStyles.container]}>
-                <Text>Profile Screen</Text>
-              </View>
-            </ImageBackground>
-          );
-        
+  return (
+    <ImageBackground
+      source={require('../assets/images/DarkBG.png')}
+      style={globalStyles.backgroundImage}
+      resizeMode="cover"
+    >
+      <Header
+        headerTitle={"Profile"}
+        backgroundColor='#00B69B'
+      />
+
+      <View style={styles.container}>
+        {/* Profile Picture */}
+        <Image
+          source={require('../assets/images/profile.jpg')}
+          style={styles.profilePicture}
+        />
+
+        {/* Pressable Titles */}
+        <PressableTitle title="Manage Account" onPress={() => handleMenuItemPress('Manage Account')} />
+        <PressableTitle title="Manage Users" onPress={() => handleMenuItemPress('Manage Users')} />
+        <PressableTitle title="Settings" onPress={() => handleMenuItemPress('Settings')} />
+        <PressableTitle title="Logout" onPress={() => handleMenuItemPress('Logout')} />
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profilePicture: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+});
+
+function handleMenuItemPress(item: string) {
+
+  switch (item) {
+    case 'Manage Account':
+
+      break;
+    case 'Manage Users':
+
+      break;
+    case 'Settings':
+  
+      break;
+    case 'Logout':
+
+      break;
+    default:
+      break;
+  }
 }
