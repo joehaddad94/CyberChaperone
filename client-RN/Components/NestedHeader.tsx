89f0cbe1c268087ce36368a041bbecbcf8318f04
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-20 21:39:35 +0300

    client <> Added back button to the nested header component

--- client-RN/Components/NestedHeader.tsx
+++ client-RN/Components/NestedHeader.tsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
-
+import { useNavigation, NavigationProp } from '@react-navigation/native';
 import globalStyles from '../styles';
+import { Ionicons } from '@expo/vector-icons';
+
+
 
 interface NestedHeaderProps {
     headerTitle: string;
     backgroundColor: string;
   }
   
-  const Header: React.FC<NestedHeaderProps> = ({ headerTitle, backgroundColor}) => {
+  const NestedHeader: React.FC<NestedHeaderProps> = ({ headerTitle, backgroundColor}) => {
+    const navigation = useNavigation();
+
+    const handleBackButtonPress = () => {
+        navigation.goBack();
+      };
+
     return (
       <View style={[styles.headerContainer, { backgroundColor }]}>
+        <TouchableOpacity onPress={handleBackButtonPress}>
+            <Ionicons name="arrow-back-sharp" size={40} color="white" /> 
+          </TouchableOpacity>
         <Text style={globalStyles.headerTitle}> {headerTitle} </Text>
     </View>
   );
   };
-
   
-export default Header
+export default NestedHeader
 
 const styles = StyleSheet.create({
   headerContainer: {
