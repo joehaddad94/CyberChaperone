e1ea577adf09b6f996ed8319e8a0a298865a323c
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-20 23:46:56 +0300

    client <> Working on manage Porfile Screen

--- client-RN/Screens/ManageProfileScreen.tsx
+++ client-RN/Screens/ManageProfileScreen.tsx
@@ -1,10 +1,85 @@
+import React from 'react';
 import { ImageBackground, Image, StyleSheet, View, Text } from 'react-native';
+import NestedHeader from '../Components/NestedHeader';
+import globalStyles from '../styles';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TextInput } from 'react-native-paper';
 
+const ManageProfileScreen: React.FC = () => {
+  return (
+    <SafeAreaView style = {globalStyles.container}>
+    <ImageBackground
+      source={require('../assets/images/DarkBG.png')}
+      style={globalStyles.backgroundImage}
+      resizeMode="cover"
+    >
+      <NestedHeader
+        headerTitle={'Settings'}
+        backgroundColor="#00B69B"
+        showButton={false}
+      />
 
-export default function ManageProfileScreen() {
-    return(
-        <View>
-            <Text>ManageProfile Screen</Text>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <Image
+            source={require('../assets/images/user.png')}
+            style={styles.profilePicture}
+          />
+          <Text style={styles.username}>Username</Text>
+          <Text style={styles.email}>email</Text>
         </View>
-    );
-}
+        <View style={styles.topContainer}>
+            <TextInput
+                label="First Name"
+                placeholder="Enter your First Name"
+                value={username}
+                onChangeText={handleUsernameChange}
+                inputStyle={styles.inputStyle}
+                secureTextEntry={false}
+              />
+            <TextInput 
+                label="Last Name"
+                placeholder="Enter your Last Name"
+                value={username}
+                onChangeText={handleUsernameChange}
+                inputStyle={styles.inputStyle}
+                secureTextEntry={false}
+            />
+        </View>
+      </View>
+    </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profilePicture: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 20,
+  },
+  topContainer: {
+    marginBottom: 40,
+    alignItems: 'center',
+  },
+  botContainer: {
+    marginBottom: 20,
+  },
+  username: {
+    color: 'white',
+    fontSize: 18,
+  },
+  email: {
+    color: 'white',
+    fontSize: 16,
+    paddingTop: 5,
+  },
+});
+
+export default ManageProfileScreen;
