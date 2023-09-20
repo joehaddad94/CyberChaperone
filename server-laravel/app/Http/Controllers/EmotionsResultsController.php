629f064d307363e989050e5c15e1db95cd8e6534
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-20 13:36:21 +0300

    server <> Minor change on the saveEmotions function

--- server-laravel/app/Http/Controllers/EmotionsResultsController.php
+++ server-laravel/app/Http/Controllers/EmotionsResultsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\DetectionResult;
+use Illuminate\Support\Facades\Auth;
 
 class emotionsResultsController extends Controller {
     public function saveEmotions(Request $request) {
