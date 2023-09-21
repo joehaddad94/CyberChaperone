d8f5197928f10bdb5da0e623a0346339d41d691e
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-22 00:03:33 +0300

    client <> Styling create user screen

--- client-RN/Screens/CreateUserScreen.tsx
+++ client-RN/Screens/CreateUserScreen.tsx
@@ -1,15 +1,23 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import globalStyles from "../styles";
 import NestedHeader from "../Components/NestedHeader";
+import { ImageBackground, View, Text } from 'react-native';
+
 
 export default function CreateUserScreen () {
     return(
         <SafeAreaView style = {globalStyles.container}>
+            <ImageBackground
+            source={require('../assets/images/DarkBG.png')}
+            style={globalStyles.backgroundImage}
+            resizeMode="cover"
+            >
             <NestedHeader
                 headerTitle = "Create User"
                 backgroundColor= "#00B69B"
                 showButton= {false}
             />
+            </ImageBackground>
         </SafeAreaView>
     )
 }
--- client-RN/Screens/UsersScreen.tsx
+++ client-RN/Screens/UsersScreen.tsx
@@ -1,11 +1,20 @@
 import { ImageBackground, Image, StyleSheet, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation, NavigationProp  } from '@react-navigation/native';
 import NestedHeader from '../Components/NestedHeader';
 import globalStyles from '../styles';
+import { StackParamList } from '../ParamTypes';
 import SearchBar from '../Components/SearchBar';
 
 
 export default function UsersScreen() {
+    const navigation = useNavigation<NavigationProp<StackParamList>>();
+
+  const handleCreateUserPress = () => {
+    // Navigate to the CreateUserScreen
+    navigation.navigate('CreateUsersScreen');
+  };
+
     return(
     <SafeAreaView style = {globalStyles.container}>
         <ImageBackground
@@ -17,6 +26,7 @@ export default function UsersScreen() {
             headerTitle={'Users'}
             backgroundColor="#00B69B"
             showButton={true}
+            onButtonPress={handleCreateUserPress}
         />
         </ImageBackground>
     </SafeAreaView>
