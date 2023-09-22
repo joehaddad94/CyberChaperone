dea8e4538e42c488d12ac9e2387aee8e6a108587
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-22 14:53:14 +0300

    client <> Added dropdown list component to dashboard

---
+++ client-RN/Components/DropdownList.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { SelectList } from 'react-native-dropdown-select-list';
+
+interface SelectListProps {
+    selectedValue: string | null;
+    setSelectedValue: (value: string | null) => void;
+}
+
+const DropdownList: React.FC<SelectListProps> = ({
+    selectedValue,
+    setSelectedValue,
+}) => {
+  const data = [
+    { key: '1', value: 'Mobiles', disabled: true },
+    { key: '2', value: 'Appliances' },
+    { key: '3', value: 'Cameras' },
+    { key: '4', value: 'Computers', disabled: true },
+    { key: '5', value: 'Vegetables' },
+    { key: '6', value: 'Diary Products' },
+    { key: '7', value: 'Drinks' },
+  ];
+
+  return (
+    <View>
+      <SelectList 
+        data={data}
+        save="value" 
+        setSelected={setSelectedValue}
+        
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  // Add your custom styles here if needed
+});
+
+export default DropdownList;
--- client-RN/Screens/DashboardScreen.tsx
+++ client-RN/Screens/DashboardScreen.tsx
@@ -1,16 +1,23 @@
 import { ImageBackground, Image, StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
 import Header from '../Components/Header'
 import SwipeCalendar from '../Components/SwipeCalendar';
+import DropdownList from '../Components/DropdownList'
 import globalStyles from '../styles';
 
 
 export default function DashboardScreen() {
+    const [selectedValue, setSelectedValue] = useState<string | null>(null);
     return(
         <View style={[globalStyles.backgroundImage,globalStyles.primaryColor]}>
              <Header 
                 headerTitle={"Dashboard"}
                 backgroundColor= '#00B69B'
             /> 
+            <DropdownList 
+                  selectedValue={selectedValue}
+                  setSelectedValue={setSelectedValue}
+            />
             <SwipeCalendar/>
         </View>
         
