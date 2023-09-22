ec94bedcaf98231a5d737306516af7c14c0c3e9a
Joe Haddad <joehaddad94@gmail.com>
2023-09-23 00:43:59 +0100

    client <> Working on fetching users in dropdown list

--- client-RN/Components/DropdownList.tsx
+++ client-RN/Components/DropdownList.tsx
@@ -2,24 +2,26 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { SelectList } from 'react-native-dropdown-select-list';
 
-interface SelectListProps {
-    selectedValue: string | null;
-    setSelectedValue: (value: string | null) => void;
+interface DropdownListProps {
+  data: { key: string; value: string }[];
+  selectedValue: string | null;
+  setSelectedValue: (value: string | null) => void;
 }
 
-const DropdownList: React.FC<SelectListProps> = ({
+const DropdownList: React.FC<DropdownListProps> = ({
+    data,
     selectedValue,
     setSelectedValue,
 }) => {
-  const data = [
-    { key: '1', value: 'Mobiles', disabled: true },
-    { key: '2', value: 'Appliances' },
-    { key: '3', value: 'Cameras' },
-    { key: '4', value: 'Computers', disabled: true },
-    { key: '5', value: 'Vegetables' },
-    { key: '6', value: 'Diary Products' },
-    { key: '7', value: 'Drinks' },
-  ];
+  // const data = [
+  //   { key: '1', value: 'Mobiles', disabled: true },
+  //   { key: '2', value: 'Appliances' },
+  //   { key: '3', value: 'Cameras' },
+  //   { key: '4', value: 'Computers', disabled: true },
+  //   { key: '5', value: 'Vegetables' },
+  //   { key: '6', value: 'Diary Products' },
+  //   { key: '7', value: 'Drinks' },
+  // ];
 
   return (
     <View>
--- client-RN/Screens/DashboardScreen.tsx
+++ client-RN/Screens/DashboardScreen.tsx
@@ -1,23 +1,57 @@
 import { ImageBackground, Image, StyleSheet, Text, View, ScrollView } from 'react-native';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from '../Components/Header'
 import SwipeCalendar from '../Components/SwipeCalendar';
 import DropdownList from '../Components/DropdownList'
 import globalStyles from '../styles';
 import PieChartComponent from '../Components/PieChartComponent';
+import axios from 'axios';
+import { BASE_URL } from '../react-native.config';
+import { useAuth } from '../ContextFiles/AuthContext';
 
+interface User {
+  username: string;
+  id: number;
+}
 
 export default function DashboardScreen() {
     const [selectedValue, setSelectedValue] = useState<string | null>(null);
+    const [users, setUsers] = useState<User[]>([])
+    const user = useAuth();
+
+    useEffect(() => {
+      fetchAllUsers();
+    }, []);
+
+    async function fetchAllUsers() {
+      try {
+          const authToken = user.user.token;
+          const apiUrl = `${BASE_URL}/api/fetch_all_users`;
+
+          const response = await axios.get(apiUrl, {
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json; charset=utf-8',
+                  'Authorization': `Bearer ${authToken}`
+              }
+          });
+          console.log(response.data)
+          setUsers(response.data.users);
+      } catch (error) {
+          console.log(error);
+      }
+  }
+
     return(
         <ScrollView style={[globalStyles.backgroundImage,globalStyles.primaryColor]}>
             <Header 
                 headerTitle={"Dashboard"}
                 backgroundColor= '#00B69B'
             /> 
-            <DropdownList 
-                selectedValue={selectedValue}
-                setSelectedValue={setSelectedValue}
+            <DropdownList
+              data={users.map(user => ({ key: user.id.toString(), value: user.username }))}
+              selectedValue={selectedValue}
+              setSelectedValue={setSelectedValue}
             />
             <SwipeCalendar/>
             
