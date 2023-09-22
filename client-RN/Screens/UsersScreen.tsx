59881a00ea2294b23febefb3333d1ebefd840b0f
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-22 12:35:04 +0300

    client <> List of users displayed

--- client-RN/Screens/UsersScreen.tsx
+++ client-RN/Screens/UsersScreen.tsx
@@ -1,15 +1,14 @@
+import React, { useState, useEffect } from 'react';
 import { ImageBackground, Image, StyleSheet, View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useNavigation, NavigationProp  } from '@react-navigation/native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
 import NestedHeader from '../Components/NestedHeader';
 import globalStyles from '../styles';
 import { StackParamList } from '../ParamTypes';
 import { AntDesign } from '@expo/vector-icons';
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../react-native.config';
 import { useAuth } from '../ContextFiles/AuthContext';
-import SearchBar from '../Components/SearchBar';
 
 interface User {
     email: string;
@@ -20,80 +19,84 @@ interface User {
 export default function UsersScreen() {
     const navigation = useNavigation<NavigationProp<StackParamList>>();
     const user = useAuth();
-    const [users, setUsers] = useState<User[]>([]); 
+    const [users, setUsers] = useState<User[]>([]);
 
-  const handleCreateUserPress = () => {
-    navigation.navigate('CreateUsersScreen');
-  };
+    const handleCreateUserPress = () => {
+        navigation.navigate('CreateUsersScreen');
+    };
 
-  useEffect(() => {
-    console.log(users)
-    async function fetchAllUsers () {
+    async function fetchAllUsers() {
         try {
             const authToken = user.user.token;
             const apiUrl = `${BASE_URL}/api/fetch_all_users`;
-    
+
             const response = await axios.get(apiUrl, {
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json; charset=utf-8',
                     'Authorization': `Bearer ${authToken}`
-                }})
-                setUsers(response.data)
-                console.log(users) 
+                }
+            });
+            console.log(response.data)
+            setUsers(response.data.users);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
-    fetchAllUsers();
-  },[users])
 
-    return(
-    <SafeAreaView style = {globalStyles.container}>
-        <ImageBackground
-            source={require('../assets/images/DarkBG.png')}
-            style={globalStyles.backgroundImage}
-            resizeMode="cover"
-        >
-        <NestedHeader
-            headerTitle={'Users'}
-            backgroundColor="#00B69B"
-            showButton={true}
-            onButtonPress={handleCreateUserPress}
-        />
-        <ScrollView style = {[globalStyles.primaryColor, styles.scrollView]}>
-            {users.map((user, index) =>(
-                
-            <View style = {styles.parentContainer} key={user.id}>
-                <View style= {styles.leftContainer}>
-                    <Image
-                        source={require('../assets/images/user.png')}
-                        style={styles.profilePicture}
-                    />
-                    <View style = {styles.textContainer}>
-                        <Text style = {styles.text}>{user.username}</Text>
-                        <Text style = {styles.text}>{user.email}</Text>
-                    </View>
-                </View>  
-                <View style={styles.buttonContainer} >
-                    <TouchableOpacity
-                    // onPress={() => handleEditUser(user.id)}
-                        style={styles.editButton}
-                    >
-                        <AntDesign name="edit" size={24} color="#00BFA4" />
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                    // onPress={() => handleDeleteUser(user.id)}
-                        style={styles.deleteButton}
-                    >
-                        <AntDesign name="delete" size={24} color="red" />
-                    </TouchableOpacity>
-                </View>    
-            </View>
-            ) )}
-        </ScrollView>
-        </ImageBackground>
-    </SafeAreaView>
+    useEffect(() => {
+        fetchAllUsers();
+    }, []);
+
+    return (
+        <SafeAreaView style={globalStyles.container}>
+            <ImageBackground
+                source={require('../assets/images/DarkBG.png')}
+                style={globalStyles.backgroundImage}
+                resizeMode="cover"
+            >
+                <NestedHeader
+                    headerTitle={'Users'}
+                    backgroundColor="#00B69B"
+                    showButton={true}
+                    onButtonPress={handleCreateUserPress}
+                />
+                <ScrollView style={[globalStyles.primaryColor, styles.scrollView]}>
+                    {users.length > 0 ? (
+                        users.map((user, index) => (
+                            <View style={styles.parentContainer} key={user.id}>
+                                <View style={styles.leftContainer}>
+                                    <Image
+                                        source={require('../assets/images/user.png')}
+                                        style={styles.profilePicture}
+                                    />
+                                    <View style={styles.textContainer}>
+                                        <Text style={styles.username}>{user.username}</Text>
+                                        <Text style={styles.email}>{user.email}</Text>
+                                    </View>
+                                </View>
+                                <View style={styles.buttonContainer} >
+                                    <TouchableOpacity
+                                        // onPress={() => handleEditUser(user.id)}
+                                        style={styles.editButton}
+                                    >
+                                        <AntDesign name="edit" size={24} color="#00BFA4" />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        // onPress={() => handleDeleteUser(user.id)}
+                                        style={styles.deleteButton}
+                                    >
+                                        <AntDesign name="delete" size={24} color="red" />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        ))
+                    ) : (
+                        <Text>No users available</Text>
+                    )}
+                </ScrollView>
+            </ImageBackground>
+        </SafeAreaView>
     );
 }
 
@@ -103,27 +106,32 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 650,
         bottom: 0,
-        borderTopRightRadius:20,
-        borderTopLeftRadius:20,
+        borderTopRightRadius: 20,
+        borderTopLeftRadius: 20,
     },
     parentContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        marginTop: 15,
+        marginTop: 30,
         paddingLeft: 25,
-        paddingRight:20,
+        paddingRight: 20,
     },
     leftContainer: {
         flexDirection: 'row',
         alignItems: 'center',
         gap: 25,
     },
-    textContainer :{
-        gap:10,
+    textContainer: {
+        gap: 10,
     },
-    text: {
+    username: {
         fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    email: {
+        fontSize: 14,
         fontWeight: 'bold',
         color: 'white',
     },
@@ -136,10 +144,10 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         gap: 20
     },
-        editButton: {
+    editButton: {
 
-        },
-        deleteButton: {
+    },
+    deleteButton: {
 
-        },
-})
+    },
+});
