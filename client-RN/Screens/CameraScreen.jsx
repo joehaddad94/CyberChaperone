20c4863757d681c2e5f1c7bc6a6385f8f03b57bf
Joe Haddad <joehaddad94@gmail.com>
2023-09-23 13:11:18 +0100

    client <> Added open camera button

--- client-RN/Screens/CameraScreen.jsx
+++ client-RN/Screens/CameraScreen.jsx
@@ -1,4 +1,108 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect, useCallback } from 'react';
+// import { Camera, CameraType } from 'expo-camera';
+// import { Button, StyleSheet, Text, TouchableOpacity, View, ImageBackground } from 'react-native';
+// import { useFocusEffect } from '@react-navigation/native'; // Import the focus effect hook
+// import globalStyles from '../styles';
+
+// const bgImage = require("../assets/images/DarkBG.png");
+
+// export default function CameraScreen({ navigation }) {
+//   const [cameraOpen, setCameraOpen] = useState(true); // Open the selfie camera by default
+//   const [type] = useState(CameraType.front); // Set the camera type to front (selfie)
+//   const [permission, requestPermission] = Camera.useCameraPermissions();
+
+//   useEffect(() => {
+//     // Cleanup function when the component unmounts
+//     return () => {
+//       // Release the camera resources when the component unmounts
+//       if (cameraOpen) {
+//         setCameraOpen(false);
+//       }
+//     };
+//   }, []);
+
+//   useFocusEffect(
+//       useCallback(() => {
+//       // This callback runs when the screen is focused
+//       setCameraOpen(true); // Open the camera when the screen is focused
+//       return () => {
+//         // This callback runs when the screen is unfocused
+//         // Close the camera when the screen is unfocused
+//         if (cameraOpen) {
+//           setCameraOpen(false);
+//         }
+//       };
+//     }, [])
+//   );
+
+//   if (!permission) {
+//     // Camera permissions are still loading
+//     return <View />;
+//   }
+
+//   if (!permission.granted) {
+//     // Camera permissions are not granted yet
+//     return (
+//       <View style={styles.container}>
+//         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+//         <Button onPress={requestPermission} title="Grant Permission" />
+//       </View>
+//     );
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <ImageBackground
+//         source={bgImage}
+//         style={globalStyles.backgroundImage}
+//         resizeMode='cover'
+//       >
+//         {cameraOpen ? (
+//           <Camera style={styles.camera} type={type}>
+//             <View style={styles.buttonContainer}>
+//               <TouchableOpacity style={styles.button} onPress={() => setCameraOpen(false)}>
+//                 <Text style={styles.text}>Close Camera</Text>
+//               </TouchableOpacity>
+//             </View>
+//           </Camera>
+//         ) : (
+//           <View style={styles.buttonContainer}>
+//             <Button onPress={() => setCameraOpen(true)} title="Open Camera" />
+//           </View>
+//         )}
+//       </ImageBackground>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//   },
+//   camera: {
+//     flex: 1,
+//   },
+//   buttonContainer: {
+//     height: '100%',
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'transparent',
+//   },
+//   button: {
+//     flex: 1,
+//     alignSelf: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     color: 'white',
+//   },
+// });
+
+import React, { useState, useEffect, useCallback } from 'react';
 import { Camera, CameraType } from 'expo-camera';
 import { Button, StyleSheet, Text, TouchableOpacity, View, ImageBackground } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native'; // Import the focus effect hook
@@ -7,7 +111,7 @@ import globalStyles from '../styles';
 const bgImage = require("../assets/images/DarkBG.png");
 
 export default function CameraScreen({ navigation }) {
-  const [cameraOpen, setCameraOpen] = useState(true); // Open the selfie camera by default
+  const [cameraOpen, setCameraOpen] = useState(false); // Initialize as closed
   const [type] = useState(CameraType.front); // Set the camera type to front (selfie)
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
@@ -22,9 +126,9 @@ export default function CameraScreen({ navigation }) {
   }, []);
 
   useFocusEffect(
-    React.useCallback(() => {
+    useCallback(() => {
       // This callback runs when the screen is focused
-      setCameraOpen(true); // Open the camera when the screen is focused
+      // If the camera is open, keep it open; otherwise, leave it closed
       return () => {
         // This callback runs when the screen is unfocused
         // Close the camera when the screen is unfocused
@@ -32,7 +136,7 @@ export default function CameraScreen({ navigation }) {
           setCameraOpen(false);
         }
       };
-    }, [])
+    }, [cameraOpen]) // Include cameraOpen in the dependencies to react to its changes
   );
 
   if (!permission) {
@@ -50,6 +154,14 @@ export default function CameraScreen({ navigation }) {
     );
   }
 
+  function openCamera() {
+    setCameraOpen(true); // Open the camera when the button is pressed
+  }
+
+  function closeCamera() {
+    setCameraOpen(false);
+  }
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -60,14 +172,14 @@ export default function CameraScreen({ navigation }) {
         {cameraOpen ? (
           <Camera style={styles.camera} type={type}>
             <View style={styles.buttonContainer}>
-              <TouchableOpacity style={styles.button} onPress={() => setCameraOpen(false)}>
+              <TouchableOpacity style={styles.button} onPress={closeCamera}>
                 <Text style={styles.text}>Close Camera</Text>
               </TouchableOpacity>
             </View>
           </Camera>
         ) : (
           <View style={styles.buttonContainer}>
-            <Button onPress={() => setCameraOpen(true)} title="Open Camera" />
+            <Button onPress={openCamera} title="Open Camera" />
           </View>
         )}
       </ImageBackground>
