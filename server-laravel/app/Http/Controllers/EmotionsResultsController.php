296826f1653978c8262f332d8e27bd59df98b4e8
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-24 19:28:14 +0300

    server <> Added max percentage emotion to data analysis function

--- server-laravel/app/Http/Controllers/EmotionsResultsController.php
+++ server-laravel/app/Http/Controllers/EmotionsResultsController.php
@@ -7,6 +7,7 @@ use App\Models\DetectionResult;
 use Illuminate\Support\Facades\Auth;
 
 class emotionsResultsController extends Controller {
+
     public function saveEmotions(Request $request) {
         try {
 
@@ -38,66 +39,84 @@ class emotionsResultsController extends Controller {
 
     public function fetchDailyDataAnalysis(Request $request) {
 
-    try {
-        $authUser = Auth::user();
+        try {
+            $authUser = Auth::user();
 
-        if (!$authUser) {
-            return response()->json(['error' => 'User is not authenticated.'], 401);
-        }
+            if (!$authUser) {
+                return response()->json(['error' => 'User is not authenticated.'], 401);
+            }
 
-        $validateData = $request->validate([
-            'userId' => 'required|numeric',
-            'timestamp' => 'required|date',
-        ]);
+            $validateData = $request->validate([
+                'userId' => 'required|numeric',
+                'timestamp' => 'required|date',
+            ]);
 
-        $Date = $validateData['timestamp'];
+            $Date = $validateData['timestamp'];
 
-        $adjustedDate = date('Y-m-d', strtotime($Date . ' +1 day'));
+            $adjustedDate = date('Y-m-d', strtotime($Date . ' +1 day'));
 
-        $userId = $validateData['userId'];
-        $detectionResults = DetectionResult::where('user_id', $userId)
-            ->whereDate('detection_time', '=', $adjustedDate)
-            ->get();
+            $userId = $validateData['userId'];
+            $detectionResults = DetectionResult::where('user_id', $userId)
+                ->whereDate('detection_time', '=', $adjustedDate)
+                ->get();
 
-        $results = [];
-        $totalEmotions = count($detectionResults);
+            $results = [];
+            $totalEmotions = count($detectionResults);
 
-        foreach ($detectionResults as $result) {
-            $detectionTime = $result->detection_time;
-            $emotionsPercentage = json_decode($result->emotions_percentage, true);
+            foreach ($detectionResults as $result) {
+                $detectionTime = $result->detection_time;
+                $emotionsPercentage = json_decode($result->emotions_percentage, true);
 
-            $results[] = [
-                'detection time' => $detectionTime,
-                'emotionPercentage' => $emotionsPercentage,
-            ];
-        }
+                $results[] = [
+                    'detection time' => $detectionTime,
+                    'emotionPercentage' => $emotionsPercentage,
+                ];
+            }
 
-        $averageEmotions = [];
+            //Calculating the average emotions percentage
+            $averageEmotions = [];
 
-        if ($totalEmotions > 0) {
-            $sumEmotions = array_fill_keys(array_keys($results[0]['emotionPercentage']), 0);
+            if ($totalEmotions > 0) {
+                $sumEmotions = array_fill_keys(array_keys($results[0]['emotionPercentage']), 0);
 
-            foreach ($results as $result) {
-                foreach ($result['emotionPercentage'] as $emotion => $percentage) {
-                    $sumEmotions[$emotion] += $percentage;
+                foreach ($results as $result) {
+                    foreach ($result['emotionPercentage'] as $emotion => $percentage) {
+                        $sumEmotions[$emotion] += $percentage;
+                    }
+                }
+
+                foreach ($sumEmotions as $emotion => $sum) {
+                    $averageEmotions[$emotion] = round($sum / $totalEmotions, 2);
                 }
             }
 
-            foreach ($sumEmotions as $emotion => $sum) {
-                $averageEmotions[$emotion] = round($sum / $totalEmotions, 2);
+            // Calculating max percentage of each emotion
+            $maxEmotions = [];
+
+            if ($totalEmotions > 0) {
+                foreach ($results[0]['emotionPercentage'] as $emotion => $percentage) {
+                    $maxEmotions[$emotion] = $percentage;
+                }
+
+                foreach ($results as $result) {
+                    foreach ($result['emotionPercentage'] as $emotion => $percentage) {
+                        if ($percentage > $maxEmotions[$emotion]) {
+                            $maxEmotions[$emotion] = $percentage;
+                        }
+                    }
+                }
             }
+
+            $responseData = [
+                'message' => 'Data retrieved successfully',
+                'averageEmotions' => $averageEmotions,
+                'maxEmotions' => $maxEmotions,
+            ];
+
+            return response()->json($responseData, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
         }
-
-        $responseData = [
-            'message' => 'Data retrieved successfully',
-            'averageEmotions' => $averageEmotions,
-            // 'results' => $results,
-        ];
-
-        return response()->json($responseData, 200);
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 500);
     }
-}
 
 }
