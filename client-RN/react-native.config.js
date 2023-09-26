d6b9357562c41b3ce568a243c179894ddb4aedad
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-26 23:26:33 +0300

    client <> Changed baseUrl to public Url

--- client-RN/react-native.config.js
+++ client-RN/react-native.config.js
@@ -4,4 +4,5 @@ export const project = {
 };
 export const assets = ['./assets/fonts/'];
 
-export const BASE_URL = 'http://10.0.2.2:8000'
+// export const BASE_URL = 'http://10.0.2.2:8000'
+export const BASE_URL = 'http://3.8.110.156'
