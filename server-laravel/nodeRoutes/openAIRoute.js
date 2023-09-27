02b516a6ff961bbc0223ba070b9c5001f385e874
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-09-27 18:24:05 +0300

    server <> adjusting openAIRoute

--- server-laravel/nodeRoutes/openAIRoute.js
+++ server-laravel/nodeRoutes/openAIRoute.js
@@ -1,27 +1,25 @@
 import express from "express";
 const router = express.Router();
 import axios from "axios";
+import { Configuration, OpenAIApi } from "openai";
+
+const config = new Configuration({
+    apiKey: "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"
+})
+
+const openai = new OpenAIApi(config);
 
 router.post("/openai", async (req, res) => {
     try {
-            console.log('entered')
-        const openaiResponse = await axios.post(
-            "https://api.openai.com/v1/engines/davinci-codex/completions",
-            {
-                prompt: "Translate the following English text to French: 'Hello, world!'",
-                max_tokens: 50,
-            },
-            {
-                headers: {
-                    Authorization: `Bearer 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`,
-                    "Content-Type": "application/json",
-                },
-            }
-        );
+        const prompt = "PHP is"
 
-        const responseData = openaiResponse.data;
-
-        res.json(responseData);
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 100,
+            temperature: 1,
+        });
+        console.log(response.data)
     } catch (error) {
         console.error("Error calling OpenAI API:", error.message);
         res.status(500).json({ error: "Error calling OpenAI API" });
